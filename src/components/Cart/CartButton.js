66880e929cd7eb7ecdfc1108f9f0a94ea10eb8ef
Kelvin <kelvinjune2@chaka.ng>
2021-12-21 20:49:28 +0100

    Added items and ui store

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,20 @@
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const { totalQuantity: cartQuantity } = useSelector((state) => state.cart);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
